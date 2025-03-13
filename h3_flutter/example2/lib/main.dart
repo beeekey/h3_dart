b250954f65f3db5a846d35a49bbeebfc34bd3e43
Ben <bekoch@gmail.com>
2025-03-13 17:54:19 +0100

    example2

---
+++ h3_flutter/example2/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:h3_flutter/h3_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final h3 = const H3Factory().load();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      restorationScopeId: "h3_flutter_example",
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Plugin example app')),
+        body: Column(
+          children: [
+            Text('180 degrees in radians is ${h3.degsToRads(180)}'),
+            Text(
+              '${h3.degsToRads(180)}',
+              key: const ValueKey('degsToRadsText'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
